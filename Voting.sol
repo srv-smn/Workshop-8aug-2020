fb4b5f0d08e571c12e12855be731df80c47e414b
sourav suman <saurav2fly@gmail.com>
2020-08-11 18:48:14 +0530

    Done with voting contract

--- Voting.sol
+++ Voting.sol
@@ -7,6 +7,10 @@ contract Voting {
     bool public stop_party_registration = false ;
     address public ewinner ;
     
+    constructor() public {
+        
+        Commissioner = msg.sender;
+    }
     
     struct Candidate{
         uint Votes;
@@ -20,9 +24,74 @@ contract Voting {
         address candidate ;
     }
     
-    constructor() public {
+    address[] public candidate ;
+    mapping(address => Candidate) public Registered;
+    mapping(address => Voter) public Voters_registered;
+    
+    function Register_Candidate(string memory Party) public {
+        require(msg.sender != Commissioner);
+        require(stop_party_registration == false);
+        require(Voting_end == true);
+        Candidate storage x = Registered[msg.sender];
+        require(x.Registered == false);
+        x.Votes = 0;
+        x.party = Party;
+        x.Address = msg.sender;
+        x.Registered = true;
+        candidate.push(msg.sender);
+    }
+    
+    
+    function Register_Vote(address addr)public {
+        require(msg.sender != Commissioner);
+        require(Voting_end == false);
+        require(msg.sender!= addr);
         
-        Commissioner = msg.sender;
+        Voter storage x = Voters_registered[msg.sender];
+        require(x.vote == false);
+        Candidate storage y = Registered[addr];
+        x.vote = true;
+        x.candidate = addr;
+        y.Votes +=1 ;
+    }
+    
+    function Start_voting() public {
+        require(msg.sender == Commissioner);
+        require(Voting_end == true);
+        Voting_end = false;
+    }
+    
+    function End_voting() public {
+        require(msg.sender == Commissioner);
+        require(Voting_end == false);
+        Voting_end = true;
+        Set_Winner();
+    }
+    
+    function Set_Winner() private {
+        uint maxVotes ;
+        address winner ;
+        uint len = candidate.length;
+        for(uint i =0; i<len ;i++){
+            if(Registered[candidate[i]].Votes > maxVotes)
+            {
+                maxVotes = Registered[candidate[i]].Votes;
+                winner = candidate[i];
+            }
+        }
+        ewinner = winner ;
+    }
+    
+    function Start_Party_Registration() public {
+        require(msg.sender == Commissioner);
+        require(stop_party_registration == true);
+        stop_party_registration = false;
+    }
+    
+    function Stop_Party_Registration() public {
+        require(msg.sender == Commissioner);
+        require(stop_party_registration == false);
+        stop_party_registration = true;
     }
     
 }

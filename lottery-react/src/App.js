4b046dd61795060f6b26d7f657d93e085583a165
sourav suman <saurav2fly@gmail.com>
2020-08-13 18:12:58 +0530

    connected with BLOCKCHAIN

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
 
-function App() {
+class App extends Component {
+  state ={
+    manager:'',
+    players:[],
+    balance: '',
+    value:'',
+    message:''
+  };
+
+  async componentDidMount(){
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address)
+    this.setState({manager, players, balance});
+
+  }
+  render(){
   return (
     <div className="App">
-      <h1>Block Chain</h1>
+      <h1>Hello world</h1>
+      <h1>{this.state.manager}|| {this.state.players}||{this.state.balance}</h1>
     </div>
   );
+}
 }
 
 export default App;

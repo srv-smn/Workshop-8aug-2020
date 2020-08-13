c18baa5417def8b97c9f51fbb74812e03a73dc25
sourav suman <saurav2fly@gmail.com>
2020-08-13 19:03:30 +0530

    basic layout

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
 import lottery from './lottery';
+import { Segment,Header, Card, Form, Container, Button } from 'semantic-ui-react' ;
+
 
 class App extends Component {
   state ={
@@ -20,11 +22,86 @@ class App extends Component {
     this.setState({manager, players, balance});
 
   }
+// header
+HeaderExampleFloating(){
+  return (
+     <Segment>
+     <Header as ='h2' color = 'blue' textAlign = 'center'>
+     LOTTERY CONTRACT
+     </Header>
+     </Segment>
+  );
+}
+
+renderCards(){
+  const items = [
+  {
+    header: this.state.manager,
+    description:
+      'Owner/creator of the contract',
+    meta: 'MANAGER',
+  },
+  {
+    header: '0.5 ETH',
+    description:
+      'Minimum contribution to participate in the lottery',
+    meta: 'Minimum Contribution',
+  },
+  {
+    header: this.state.players.length,
+    description:
+      'Tolal number of players in the Lottery',
+    meta: 'total participants',
+  },
+  {
+    header: web3.utils.fromWei(this.state.balance,'ether'),
+    description:
+      'Total money in the lottery',
+    meta: 'Winning amount',
+  },
+]
+return <Card.Group items={items} /> ;
+
+}
+onSubmit = async event =>{
+  event.preventDefault();
+  const accounts = await web3.eth.getAccounts();
+  this.setState({message: 'Waiting for transaction success ...'});
+  await lottery.methods.enter().send({
+    from:accounts[0],
+    value: web3.utils.toWei(this.state.value,'ether')
+  });
+  this.setState({message: 'You have entered in the contract'});
+  window.location.reload(false);
+
+};
+
+
+
+
+
   render(){
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <h1>{this.state.manager}|| {this.state.players}||{this.state.balance}</h1>
+  <p>  {this.HeaderExampleFloating()} </p>
+    <p>  {this.renderCards()} </p>
+      <p>
+      <Container style = {{marginTop : '30px'}}>
+      <Card centered = {true} color = 'yellow' fluid = {true}>
+      <Form onSubmit = {this.onSubmit} size = 'big'>
+      <p> <h2 align  = 'center' color = 'blue'> Want to try your luck ? </h2></p>
+      <Form.Input placeholder = 'Amount in ether'
+      name =  'name'
+      value = {this.state.value}
+      onChange = {event => this.setState({value: event.target.value})}
+      />
+      <Button type ='submit' positive = {true} fluid = {true}> Submit </Button>
+      </Form>
+      </Card>
+      </Container>
+
+      </p>
+
     </div>
   );
 }
